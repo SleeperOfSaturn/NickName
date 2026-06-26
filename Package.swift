98032864ea8bfc042b0fe88b8ec95c12d5682317
Saturn <80062054+SleeperOfSaturn@users.noreply.github.com>
2026-06-26 07:14:03 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ import PackageDescription
 let package = Package(
     name: "NickName",
     platforms: [
-        .iOS("26.0"),
-        .macOS(.v15),
+        .iOS("26.0")
     ],
     products: [
         .library(
